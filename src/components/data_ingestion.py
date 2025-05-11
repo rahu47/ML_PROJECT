03e324e61c774d299a93d8bdb37dcdf7f0614f78
RAHUL <yalavattirahul@gmail.com>
2025-05-12 00:20:35 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from src.components.model_trainer import ModelTrainer
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
@@ -9,6 +10,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -23,7 +26,7 @@ class DataIngestion:
     def initate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset as a dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -48,8 +51,9 @@ if __name__=="__main__":
     train_data,test_data=obj.initate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
  
